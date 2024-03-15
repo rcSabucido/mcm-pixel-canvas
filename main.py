72818aa25b1a9ff76ec828c91c105d694b762b99
Ryz Clyd Sabucido <rcsabucido@gmail.com>
2024-03-15 21:37:21 +0800

    Add pygame_gui to codebase and add text box for user input on grid dimensions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+import pygame_gui
 from pygame.locals import *
 
 pygame.init()
@@ -9,37 +11,78 @@ SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+CLOCK = pygame.time.Clock()
+MANAGER = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-number_of_rows = 50
-number_of_columns = 50
-grid = [[0 for x in range(number_of_rows)] for y in range(number_of_columns)]
+TEXT_INPUT = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((100, 100),
+                                                                           (500, 50)), manager=MANAGER,
+                                                                           object_id="#main_text_entry")
+                                                 
+user_text = ""
 
-basic_x = SCREEN_WIDTH / number_of_columns
-basic_y = SCREEN_HEIGHT / number_of_rows
+def menu():
+    global user_text
+    while True:
+        UI_REFRESH_RATE = CLOCK.tick(60)/1000
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == "#main_text_entry":
+                user_text = TEXT_INPUT.get_text()
+                actual_canvas(user_text)
+
+                
+                
+            MANAGER.process_events(event)
+        
+        MANAGER.update(UI_REFRESH_RATE)
+        
+        screen.fill("white")
+
+        MANAGER.draw_ui(screen)
+
+        pygame.display.update()
 
 
-running = True
+# specifies the grid dimension of the canvas
 
-clicking = 0
+def actual_canvas(text):
+    number_of_rows = int(text)
+    number_of_columns = int(text)
+    grid = [[0 for x in range(number_of_rows)] for y in range(number_of_columns)]
 
-screen.fill((255, 255, 255))
+    # calculates the specific dimensions of the individual cells
+    basic_x = SCREEN_WIDTH / number_of_columns
+    basic_y = SCREEN_HEIGHT / number_of_rows
 
-while running:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
+    running = True
+
+    clicking = 0
+
+    screen.fill((255, 255, 255))
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+            elif event.type == QUIT:
                 running = False
-        elif event.type == QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN or clicking:
-            clicking = 1
-            x, y = pygame.mouse.get_pos()
-            x_in_grid = int(x / basic_x)
-            y_in_grid = int(y/ basic_y)
-            grid[y_in_grid][x_in_grid] = 1
-            pygame.draw.rect(screen, (0, 0, 0), (x_in_grid * basic_x,
-                                                 y_in_grid * basic_y, basic_x,
-                                                 basic_y))
-            pygame.display.flip()
-        if event.type == pygame.MOUSEBUTTONUP:
-            clicking = 0
+            if event.type == pygame.MOUSEBUTTONDOWN or clicking:
+                clicking = 1
+                x, y = pygame.mouse.get_pos()
+                x_in_grid = int(x / basic_x)
+                y_in_grid = int(y/ basic_y)
+                grid[y_in_grid][x_in_grid] = 1
+
+                # x_in_grid * basic_x gives pixel position of left edge of cell
+                # y_in_grid * basic_y gives pixel position of top edge of cell
+                pygame.draw.rect(screen, (0, 0, 0), (x_in_grid * basic_x,
+                                                    y_in_grid * basic_y, basic_x,
+                                                    basic_y)) 
+                pygame.display.flip()
+            if event.type == pygame.MOUSEBUTTONUP:
+                clicking = 0
+
+menu()
